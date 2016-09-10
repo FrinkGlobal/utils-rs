5b841f64b59803e6e77b31c80dc9f9f539dd6fb8
Iban Eguia <iban@fractal.global>
2016-09-10 19:37:42 -0400

    Added logo and favicon to docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![doc(html_logo_url = "http://test.fractal.global/img/logo.svg",
+       html_favicon_url = "http://test.fractal.global/img/favicon32.png",
+       html_root_url = "http://fractal.global/utils-rs/")]
 //! This crate contains utilities to work with Fractal Global Credits. It contains the wallet
 //! address representation and the currency amount type. Each of them is explained in its own
 //! documentation and can be easily used by third parties.
